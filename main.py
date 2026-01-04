fa311c61cff14758d70a0caddeb6c696ef4759b5
pelti <vonkyreinhenri@gmail.com>
2026-01-04 10:49:53 +0100

    chapitre 1 and tu vas pas lire donc blc

---
+++ main.py
@@ -0,0 +1,8 @@
+def __main__():
+    from utils.menu import show_menu
+    from chapters.chapter_1 import chapter_1
+    from chapters.chapter_2 import chapter_2
+    from chapters.chapter_3 import chapter_3
+    from chapters.chapter_4 import chapter_4
+
+print( __main__())
